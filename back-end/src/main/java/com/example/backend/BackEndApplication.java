abd25326d65971e202ca17375b66cfd7a0b24bd9
jack <yjk9805@naver.com>
2024-01-23 11:25:08 +0900

    feat: gerrit check

--- back-end/src/main/java/com/example/backend/BackEndApplication.java
+++ back-end/src/main/java/com/example/backend/BackEndApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackEndApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BackEndApplication.class, args);
+		String gerrit = "gerrit";
 	}
 
 }
