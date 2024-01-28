c1d0f0584e1c1163b5b39e712a3846e1e5b58c73
seojeong <sjytis14@naver.com>
2024-01-28 18:57:26 +0900

    Merge branch 'front-feat/mainpage' into front-feat/design

--- front-end/src/components/header/DefaultHeader.jsx
+++ front-end/src/components/header/DefaultHeader.jsx
@@ -10,7 +10,7 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import logo from "@/assets/logo.svg";
-import { useRef } from "react";
+
 function DefaultHeader(props) {
   return (
     <>
@@ -40,7 +40,7 @@ function DefaultHeader(props) {
                     <Search color="gray" />
                   </div>
                   <Input
-                    type="text"
+                    type="search"
                     placeholder="팀을 검색해보세요"
                     className="block w-full rounded-md border-0 py-1.5 pl-11 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
@@ -63,13 +63,12 @@ function DefaultHeader(props) {
                       <Search color="gray" />
                     </div>
                     <Input
-                      type="text"
+                      type="search"
                       placeholder="팀을 검색해보세요"
                       className="block w-full rounded-md border-0 py-1.5 pl-11 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-foreground sm:text-sm sm:leading-6"
                     />
                   </div>
                   <DrawerClose asChild>
-                    {/* <Button variant="outline">취소</Button> */}
                     <Button variant="outline">취소</Button>
                   </DrawerClose>
                 </div>
