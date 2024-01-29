ef264518dcc1d129c1ccb45228835c755b9d0029
kanggeon <gun4329@gmail.com>
2024-01-29 16:55:44 +0900

    Feat: add GeneratedToken.java

---
+++ back-end/src/main/java/com/example/backend/security/dto/GeneratedToken.java
@@ -0,0 +1,16 @@
+package com.example.backend.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@Builder
+@ToString
+public class GeneratedToken {
+
+    private String accessToken;
+    private String refreshToken;
+}
