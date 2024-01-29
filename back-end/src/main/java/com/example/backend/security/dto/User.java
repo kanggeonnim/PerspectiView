c31094caeb2d6bd37084a7456ec3f0d66dd1b060
kanggeon <gun4329@gmail.com>
2024-01-29 16:47:59 +0900

    Feat: add UserDto

---
+++ back-end/src/main/java/com/example/backend/security/dto/User.java
@@ -0,0 +1,33 @@
+package com.example.backend.security.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+// ORM - Object Relation Mapping
+
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private String role; //ROLE_USER, ROLE_ADMIN
+	// OAuth를 위해 구성한 추가 필드 2개
+	private String provider;
+	private String providerId;
+	@CreationTimestamp
+	private Timestamp createDate;
+}
