83c5ce13270f4b172d4290049e8360d7970790b2
jack <yjk9805@naver.com>
2024-01-30 15:59:14 +0900

    Feat: add TeamDto

--- back-end/src/main/java/com/example/backend/modules/team/TeamRequestDto.java
+++ back-end/src/main/java/com/example/backend/modules/team/TeamRequestDto.java
@@ -1,14 +1,23 @@
 package com.example.backend.modules.team;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
-
+@Data
+@Builder
 public class TeamRequestDto {
     @Length(max = 20) @NotNull
-    private String teamTitle;
-    private String teamInfo;
+    private String title;
+    private String info;
 
     @NotNull
     private boolean personal;
 
+    public static Team from(TeamRequestDto teamRequestDto){
+        return Team.builder().title(teamRequestDto.getTitle())
+                .info(teamRequestDto.getInfo())
+                .personal(teamRequestDto.isPersonal())
+                .build();
+    }
 }
---
+++ back-end/src/main/java/com/example/backend/modules/team/TeamResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.backend.modules.team;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class TeamResponseDto {
+    private String title;
+    private String info;
+    private boolean personal;
+
+    public static TeamResponseDto of(Team team) {
+        return TeamResponseDto.builder()
+                .title(team.getTitle())
+                .info(team.getInfo())
+                .personal(team.getPersonal())
+                .build();
+    }
+}
