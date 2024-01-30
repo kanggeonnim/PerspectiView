d829718767e7e055cc57225d4efd486b87bceeab
seojeong <sjytis14@naver.com>
2024-01-31 01:13:07 +0900

    Feat: router 수정

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,33 +1,10 @@
-import TeamWorkspacePage from "./pages/workspace/TeamWorkspacePage";
-import LoginPage from "./pages/auth/LoginPage";
-import ProductPage from "./pages/product/ProductPage";
-import MyPage from "./pages/MyPage/MyPage";
-import IndexPage from "./pages/auth/IndexPage";
-import ForeshadowingPage from "./pages/product/ForeshadowingPage";
-import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
-import MyWorkspacePage from "./pages/workspace/MyWorkspacePage";
-import CharPage from "./pages/product/CharPage";
-import routes from "./routes/routes";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const { id } = useParams();
   return (
-    <BrowserRouter>
-      <Routes>
-        {routes.map(({ path, component }) => {
-          return <Route key={path} path={path} element={component()} />;
-        })}
-        {/* <Route path="/" element={<IndexPage/>}></Route>
-        <Route path="/login" element={<LoginPage/>}></Route>
-        <Route path="/product/*" element={<ProductPage/>}></Route>
-        <Route path="/product/char" element={<CharPage/>}></Route>
-        <Route path="/foreshadowingpage" element={<ForeshadowingPage/>}></Route>
-        <Route path="/mypage/*" element={<MyPage/>}></Route>
-        <Route path="/workspace" element={<MyWorkspacePage/>}></Route>
-        <Route path="/teamWorkspacepage/*" element={<TeamWorkspacePage/>}></Route> */}
-      </Routes>
-      {id}
-    </BrowserRouter>
+    <>
+      <Outlet />
+    </>
   );
 }
 
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,11 +2,86 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import ErrorPage from "./pages/ErrorPage.jsx";
+import IndexPage from "./pages/auth/IndexPage.jsx";
+import LoginPage from "./pages/auth/LoginPage.jsx";
+import ProductPage from "./pages/product/ProductPage.jsx";
+import CharTab from "./pages/product/components/character/CharTab.jsx";
+import ForeshadowingTab from "./pages/product/components/foreshadowing/ForeshadowingTab.jsx";
+import StoryInfo from "./pages/product/components/flow/story/StoryInfo.jsx";
+import MyPage from "./pages/auth/MyPage.jsx";
+import FlowTab from "./pages/product/components/flow/FlowTab.jsx";
+import WorkspacePage from "./pages/workspace/WorkspacePage.jsx";
+import WorkListCard from "./pages/workspace/components/WorkListCard.jsx";
+import TeamWorkspaceBody from "./pages/workspace/components/TeamWorkspaceBody.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+
+    children: [
+      {
+        path: "",
+        element: <IndexPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "settings/profile",
+        element: <MyPage />,
+      },
+      {
+        path: "workspace",
+        element: <WorkspacePage />,
+        children: [
+          {
+            path: "",
+            element: <WorkListCard />,
+          },
+          {
+            path: "team/:teamId",
+            element: <TeamWorkspaceBody />,
+          },
+        ],
+      },
+      {
+        path: "product/:productId",
+        element: <ProductPage />,
+        children: [
+          {
+            path: "",
+            element: <Navigate replace to="character" />,
+          },
+          {
+            path: "character",
+            element: <CharTab />,
+          },
+          {
+            path: "foreshadowing",
+            element: <ForeshadowingTab />,
+          },
+          {
+            path: "flow",
+            element: <FlowTab />,
+          },
+          {
+            path: "story/:storyId",
+            element: <StoryInfo />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
