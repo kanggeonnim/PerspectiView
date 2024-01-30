e8f8be410eddd90ae55e4e664cd8fd9fa970af30
kanggeon <gun4329@gmail.com>
2024-01-30 10:12:17 +0900

    Feat: update JwtSuccessHandler.java
    token 전달방식을 header -> body로 교체

--- back-end/src/main/java/com/example/backend/security/jwt/JwtSuccessHandler.java
+++ back-end/src/main/java/com/example/backend/security/jwt/JwtSuccessHandler.java
@@ -2,6 +2,7 @@ package com.example.backend.security.jwt;
 
 import com.example.backend.security.config.auth.PrincipalDetails;
 import com.example.backend.security.dto.GeneratedToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,23 +17,23 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtSuccessHandler implements AuthenticationSuccessHandler {
 
-    private  final JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
     private final JwtProperties jwtProperties;
-
+    private final ObjectMapper objectMapper;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         System.out.println("successfullAuthentication 실행됨.");
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
 
         GeneratedToken token =
-                jwtUtil.generateToken(
-                        principalDetails.getUser().getEmail(),
-                        principalDetails.getUser().getRole());
+                jwtUtil.generateToken(principalDetails.getUser().getUsername(), principalDetails.getUser().getRole());
 
+        // body를 통해 access token과 refresh token 전달
+        String result = objectMapper.writeValueAsString(token);
+        response.getWriter().write(result);
 
-        response.getWriter().write("WOW");
-        response.addHeader(jwtProperties.getHaederAccess(),jwtProperties.getTokenPrefix()+token.getAccessToken());
-        response.addHeader(jwtProperties.getHaederRefresh(),jwtProperties.getTokenPrefix()+token.getRefreshToken());
-
+        // header를 통해 access token과 refresh token 전달
+//        response.addHeader(jwtProperties.getHeaderAccess(),jwtProperties.getTokenPrefix()+token.getAccessToken());
+//        response.addHeader(jwtProperties.getHeaderRefresh(),jwtProperties.getTokenPrefix()+token.getRefreshToken());
     }
 }
