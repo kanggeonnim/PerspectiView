70122eeb934e96b742bde5a8484ba1f2a028139c
jack <yjk9805@naver.com>
2024-01-30 16:01:52 +0900

    Feat: implement logic 매니저 추가

--- back-end/src/main/java/com/example/backend/modules/team/TeamController.java
+++ back-end/src/main/java/com/example/backend/modules/team/TeamController.java
@@ -1,13 +1,30 @@
 package com.example.backend.modules.team;
 
+import com.example.backend.modules.api.ApiResult;
+import com.example.backend.modules.auth.principal.PrincipalDetails;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("team")
 public class TeamController {
     private final TeamService teamService;
 
+    @PostMapping("/")
+    public ApiResult<TeamResponseDto> createTeam(@RequestBody @Valid TeamRequestDto teamRequestDto,
+                                                 @AuthenticationPrincipal PrincipalDetails principalDetails){
+
+        Team team = teamService.createTeam(TeamRequestDto.from(teamRequestDto), principalDetails.getUser());
+        // 매니저에 추가
+        return ApiResult.OK(TeamResponseDto.of(team));
+    }
 
 
 }
--- back-end/src/main/java/com/example/backend/modules/team/TeamService.java
+++ back-end/src/main/java/com/example/backend/modules/team/TeamService.java
@@ -1,13 +1,20 @@
 package com.example.backend.modules.team;
 
+import com.example.backend.modules.account.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class TeamService {
     private final TeamRepository teamRepository;
 
-
+    public Team createTeam(Team team, User user){
+        Team newTeam = teamRepository.save(team);;
+        newTeam.addManager(user);
+        return newTeam;
+    }
 
 }
