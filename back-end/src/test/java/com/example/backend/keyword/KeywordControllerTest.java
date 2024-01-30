132edd65f9e13b2d84a2afcb3fea97094522f83e
SIMJIYEON <sim.yyeon@gmail.com>
2024-01-30 22:56:13 +0900

    Test: KeywordController에 대한 Test 코드 추가

--- back-end/src/test/java/com/example/backend/keyword/KeywordControllerTest.java
+++ back-end/src/test/java/com/example/backend/keyword/KeywordControllerTest.java
@@ -1,34 +1,55 @@
 package com.example.backend.keyword;
 
-import jakarta.annotation.security.RunAs;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import com.google.gson.Gson;
+import org.apache.coyote.http11.upgrade.UpgradeServletOutputStream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.Assert.*;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
-@Transactional
-@RunWith(SpringRunner.class)
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+
+import static org.mockito.Mockito.verify;
+
+@WebMvcTest(controllers = KeywordController.class)
 public class KeywordControllerTest {
     @Autowired
-    KeywordController keywordController;
-    @Autowired
+    MockMvc mvc;
+
+    @MockBean
     KeywordService keywordService;
-    @Autowired
-    KeywordRepository keywordRepository;
 
     @Test
+    @DisplayName("키워드 생성 테스트")
     public void 키워드생성() throws Exception {
         //given
-        Keyword keyword = Keyword.builder().name("무뚝뚝").build();
-
+//        String content = objectMapper.writeValueAsString(
+//                Keyword.builder()
+//                        .name("활기찬")
+//                        .build()
+//        );
+        Keyword keyword = Keyword.builder().name("testname").build();
+        Gson gson = new Gson();
+        String content = gson.toJson(keyword);
 
         //when
-        Keyword checkkeyword =  keywordService.createKeyword(keyword);
+        mvc.perform(post("/keyword").contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().isCreated());
         //then
+        verify(keywordService).createKeyword(keyword);
     }
 }
