cc7ed7a7a322d316fcdd6407f1d00c7875c55e61
kanggeon <gun4329@gmail.com>
2024-01-30 10:33:59 +0900

    Feat: add JwtExceptionFilter.java
    Jwt인가시 Exception 처리를 위한 Filter생성

---
+++ back-end/src/main/java/com/example/backend/security/jwt/JwtExceptionFilter.java
@@ -0,0 +1,34 @@
+package com.example.backend.security.jwt;
+
+import com.example.backend.security.dto.StatusResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (JwtException e) {
+            response.setStatus(401);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            objectMapper.writeValue(response.getWriter(), StatusResponseDto.addStatus(401));
+        }
+    }
+}
