2cc9855b4c83e02f7d039358b20c94547c9b896f
kanggeon <gun4329@gmail.com>
2024-01-30 10:50:08 +0900

    Refactor: update User
    id type을 int->Long 수정

--- back-end/src/main/java/com/example/backend/security/dto/User.java
+++ back-end/src/main/java/com/example/backend/security/dto/User.java
@@ -20,7 +20,7 @@ import java.sql.Timestamp;
 public class User {
 	@Id // primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private Long id;
 	private String username;
 	private String password;
 	private String email;
