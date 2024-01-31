8eee550a47301d9139e2bab6897327f24a5211fc
jack <yjk9805@naver.com>
2024-01-31 14:12:11 +0900

    Feat: application 환경변수 암호화

---
+++ back-end/src/test/java/com/example/backend/infra/config/JasyptConfigTest.java
@@ -0,0 +1,29 @@
+package com.example.backend.infra.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JasyptConfigTest {
+
+    @Test
+    void stringEncryptor() {
+        String something = "";
+
+        System.out.println(jasyptEncoding(something));
+
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        pbeEnc.setPassword(key);
+        pbeEnc.setIvGenerator(new RandomIvGenerator());
+        return pbeEnc.encrypt(value);
+    }
+
+}
