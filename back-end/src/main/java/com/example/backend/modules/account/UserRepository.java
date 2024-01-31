a30535d4d53700033364005fd83539e5aa2ada1c
kanggeon <gun4329@gmail.com>
2024-01-31 13:20:52 +0900

    Refactor: update UserRepository
    EntityGraph를 사용한 User, UserAuth 조회코드 추가

--- back-end/src/main/java/com/example/backend/modules/account/UserRepository.java
+++ back-end/src/main/java/com/example/backend/modules/account/UserRepository.java
@@ -2,7 +2,7 @@ package com.example.backend.modules.account;
 
 import java.util.Optional;
 
-import com.example.backend.modules.account.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -16,8 +16,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // JpaRepository 를 상속하면 자동 컴포넌트 스캔됨.
 public interface UserRepository extends JpaRepository<User, Long>{
 	// SELECT * FROM user WHERE username = ?1
-	Optional<User> findByUsername(String username);
+	@EntityGraph(attributePaths = "authorities")
+	Optional<User> findWithAuthoritiesByUsername(String username);
 
+	Optional<User> findByUsername(String username);
 	// SELECT * FROM user WHERE provider = ?1 and providerId = ?2
 	Optional<User> findByProviderAndProviderId(String provider, String providerId);
 }
