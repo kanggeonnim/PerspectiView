5a1f328d1eb1fbf8efd524d3d1dc2efe5875e293
심지연 <sim.yyeon@gmail.com>
2024-01-31 15:30:43 +0900

    Refactor: modify controller to Restcontroller

--- back-end/src/main/java/com/example/backend/keyword/KeywordController.java
+++ back-end/src/main/java/com/example/backend/keyword/KeywordController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/keyword")
 public class KeywordController {
