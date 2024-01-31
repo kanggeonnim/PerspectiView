5d70b451b7eb43820a9e57a7ad67216366d95f2e
심지연 <sim.yyeon@gmail.com>
2024-01-31 13:17:47 +0900

    Feat: return 타입 ResponseEntity로 수정

--- back-end/src/main/java/com/example/backend/keyword/KeywordController.java
+++ back-end/src/main/java/com/example/backend/keyword/KeywordController.java
@@ -1,9 +1,13 @@
 package com.example.backend.keyword;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,27 +18,39 @@ public class KeywordController {
     private final KeywordService keywordService;
 
     @PostMapping("/")
-    public String createKeyword(@RequestBody KeywordRequestDto keywordRequestDto){
-        keywordService.createKeyword(keywordRequestDto.toEntity());
-        return "";
+    public ResponseEntity<HttpStatus> createKeyword(@RequestBody KeywordRequestDto keywordRequestDto){
+        Keyword keyword = keywordService.createKeyword(keywordRequestDto.toEntity());
+        if(keyword!= null){
+            return new ResponseEntity(HttpStatus.CREATED);
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("/{keywordId}")
-    public String keywordFindById(@PathVariable("keywordId") Long id){
+    public ResponseEntity<HttpStatus> keywordFindById(@PathVariable("keywordId") Long id){
         Optional<Keyword> keyword= keywordService.findById(id);
-        return "";
+        if(keyword!= null){
+            return new ResponseEntity(HttpStatus.CREATED);
+        }
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("/{keywordName}")
-    public String keywordFindByName(@PathVariable("keywordName")String name){
+    public ResponseEntity<HttpStatus> keywordFindByName(@PathVariable("keywordName")String name){
         List<Keyword> keywordList = keywordService.findKeywordByName(name);
-        return "";
+        if(keywordList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/")
-    public String keysordFindAll(){
+    public ResponseEntity<HttpStatus> keysordFindAll(){
         List<Keyword> keywordList = keywordService.findAll();
-        return "";
+        if(keywordList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
