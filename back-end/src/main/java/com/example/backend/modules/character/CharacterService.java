6c60d5fc3e105a149640431727e2a61ec8e3bb84
kanggeonnim <gun4329@gmail.com>
2024-02-01 23:54:01 +0900

    Feat: add CharacterService

--- back-end/src/main/java/com/example/backend/modules/character/CharacterService.java
+++ back-end/src/main/java/com/example/backend/modules/character/CharacterService.java
@@ -17,14 +17,28 @@ public class CharacterService {
     private final CharacterRepository characterRepository;
     private final TeamService teamService;
 
+    /**
+     * 등장인물 생성
+     * @param character
+     * @param teamId
+     * @param user
+     * @return
+     */
     @Transactional
-    public Character createCharacter(Character character, Team team, User user) {
-        teamService.checkIfManager(user, team);
+    public Character createCharacter(Character character, Long teamId, User user) {
+        teamService.checkIfManager(user, teamService.getTeam(teamId));
 
         return characterRepository.save(character);
     }
 
 
+    /**
+     * 전체 등장인물 조회
+     * @param productId
+     * @param teamId
+     * @param user
+     * @return
+     */
     public List<Character> getCharacters(Long productId, Long teamId, User user) {
         teamService.checkIfMember(user, teamService.getTeam(teamId));
 
@@ -32,22 +46,42 @@ public class CharacterService {
         return characterRepository.findAllByProductId(productId);
     }
 
-    public Character getCharacter(Long charId, Team team, User user) {
-        teamService.checkIfMember(user, team);
+    /**
+     * 단일 등장인물 조회
+     * @param charId
+     * @param teamId
+     * @param user
+     * @return
+     */
+    public Character getCharacter(Long charId, Long teamId, User user) {
+        teamService.checkIfMember(user, teamService.getTeam(teamId));
 
         // 해당 작품에 포함된 인물인지도 검사해야함.
 
         return characterRepository.findById(charId).orElseThrow(() -> new RuntimeException());
     }
 
+    /**
+     * 등장인물 정보 수정
+     * @param character
+     * @param teamId
+     * @param user
+     * @return
+     */
     @Transactional
-    public Character updateCharacter(Character character, Team team, User user) {
-        teamService.checkIfManager(user, team);
+    public Character updateCharacter(Character character, Long teamId, User user) {
+        teamService.checkIfManager(user, teamService.getTeam(teamId));
 
         Character newCharacter = characterRepository.findById(character.getId()).orElseThrow(() -> new RuntimeException());
         return newCharacter;
     }
 
+    /**
+     * 등장인물 삭제
+     * @param charId
+     * @param teamId
+     * @param user
+     */
     @Transactional
     public void deleteCharacter(Long charId, Long teamId, User user) {
         teamService.checkIfManager(user, teamService.getTeam(teamId));
