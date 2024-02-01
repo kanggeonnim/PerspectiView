ed31fc61e69e9dcdb1cabf34fb3c8ce7cac9baac
SIMJIYEON <sim.yyeon@gmail.com>
2024-02-02 00:22:45 +0900

    Feat: add 장르 리스트

--- back-end/src/main/java/com/example/backend/modules/product/ProductResponseDto.java
+++ back-end/src/main/java/com/example/backend/modules/product/ProductResponseDto.java
@@ -2,10 +2,12 @@ package com.example.backend.modules.product;
 
 import com.example.backend.modules.category.Category;
 import com.example.backend.modules.category.CategoryResponseDto;
+import com.example.backend.modules.genre.Genre;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.HashSet;
+import java.util.List;
 
 @Data
 @Builder
@@ -14,14 +16,16 @@ public class ProductResponseDto {
     private String productTitle;
     private String productInfo;
     private Category category;
+    private List<Genre> genres;
     //플롯 hashset - 스토리까지 todo entitygraph
 
-    public static ProductResponseDto from(Product product){
+    public static ProductResponseDto from(Product product, List<Genre> genres){
         return ProductResponseDto.builder()
                 .productId(product.getId())
                 .productTitle(product.getTitle())
                 .productInfo(product.getInfo())
                 .category(product.getCategory())
+                .genres(genres)
                 .build();
     }
 }
