5a40a7e107383353e60ddadee220e8b399867312
kanggeonnim <gun4329@gmail.com>
2024-02-01 23:53:47 +0900

    Feat: add CharacterController

--- back-end/src/main/java/com/example/backend/modules/character/CharacterController.java
+++ back-end/src/main/java/com/example/backend/modules/character/CharacterController.java
@@ -2,6 +2,8 @@ package com.example.backend.modules.character;
 
 import com.example.backend.modules.api.ApiResult;
 import com.example.backend.modules.auth.principal.PrincipalDetails;
+import com.example.backend.modules.team.TeamService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +24,33 @@ public class CharacterController {
                 .collect(Collectors.toList()));
     }
 
+    @GetMapping("/{characterId}")
+    public ApiResult<CharacterResponseDto> getCharacter(@PathVariable Long productId, @PathVariable Long teamId,
+                                             @AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable Long characterId){
+        Character character = characterService.getCharacter(characterId, teamId, principalDetails.getUser());
+        return ApiResult.OK(CharacterResponseDto.of(character));
+    }
+
     @DeleteMapping("/{characterId}")
-    public ApiResult<CharacterResponseDto> deleteCharacter(@PathVariable Long productId, @PathVariable Long teamId, @AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable Long characterId) {
+    public ApiResult<CharacterResponseDto> deleteCharacter(@PathVariable Long productId, @PathVariable Long teamId,
+                                                           @AuthenticationPrincipal PrincipalDetails principalDetails,
+                                                           @PathVariable Long characterId) {
         characterService.deleteCharacter(characterId, teamId, principalDetails.getUser());
         return ApiResult.OK(null);
     }
 
+    @PatchMapping("/{characterId}")
+    public ApiResult<CharacterResponseDto> updateCharacter(@PathVariable Long productId, @PathVariable Long teamId, @AuthenticationPrincipal PrincipalDetails principalDetails,
+                                                           @PathVariable Long characterId, @RequestBody @Valid CharacterRequestDto characterRequestDto){
+        Character character = characterService.updateCharacter(CharacterRequestDto.from(characterRequestDto), teamId, principalDetails.getUser());
+        return ApiResult.OK(CharacterResponseDto.of(character));
+    }
+
+    @PostMapping
+    public ApiResult<CharacterResponseDto> createCharacter(@PathVariable Long productId, @PathVariable Long teamId,
+                                                           @RequestBody @Valid CharacterRequestDto characterRequestDto,
+                                                           @AuthenticationPrincipal PrincipalDetails principalDetails){
+        Character character = characterService.createCharacter(CharacterRequestDto.from(characterRequestDto), teamId, principalDetails.getUser());
+        return ApiResult.OK(CharacterResponseDto.of(character));
+    }
 }
