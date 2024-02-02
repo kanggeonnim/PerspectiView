559f065380460a26ae12030fcc33c1d71f49edb2
kanggeon <gun4329@gmail.com>
2024-02-02 15:17:39 +0900

    Feat: add parameter(productId)
    모든 method에 작품 검증을 위한 productId 파라미터 추가

--- back-end/src/main/java/com/example/backend/modules/character/CharacterController.java
+++ back-end/src/main/java/com/example/backend/modules/character/CharacterController.java
@@ -26,8 +26,8 @@ public class CharacterController {
 
     @GetMapping("/{characterId}")
     public ApiResult<CharacterResponseDto> getCharacter(@PathVariable Long productId, @PathVariable Long teamId,
-                                             @AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable Long characterId){
-        Character character = characterService.getCharacter(characterId, teamId, principalDetails.getUser());
+                                                        @AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable Long characterId) {
+        Character character = characterService.getCharacter(characterId, productId, teamId, principalDetails.getUser());
         return ApiResult.OK(CharacterResponseDto.of(character));
     }
 
@@ -35,22 +35,22 @@ public class CharacterController {
     public ApiResult<CharacterResponseDto> deleteCharacter(@PathVariable Long productId, @PathVariable Long teamId,
                                                            @AuthenticationPrincipal PrincipalDetails principalDetails,
                                                            @PathVariable Long characterId) {
-        characterService.deleteCharacter(characterId, teamId, principalDetails.getUser());
+        characterService.deleteCharacter(characterId, productId, teamId, principalDetails.getUser());
         return ApiResult.OK(null);
     }
 
     @PatchMapping("/{characterId}")
     public ApiResult<CharacterResponseDto> updateCharacter(@PathVariable Long productId, @PathVariable Long teamId, @AuthenticationPrincipal PrincipalDetails principalDetails,
-                                                           @PathVariable Long characterId, @RequestBody @Valid CharacterRequestDto characterRequestDto){
-        Character character = characterService.updateCharacter(CharacterRequestDto.from(characterRequestDto), teamId, principalDetails.getUser());
+                                                           @PathVariable Long characterId, @RequestBody @Valid CharacterRequestDto characterRequestDto) {
+        Character character = characterService.updateCharacter(CharacterRequestDto.from(characterRequestDto), characterId, productId, teamId, principalDetails.getUser());
         return ApiResult.OK(CharacterResponseDto.of(character));
     }
 
     @PostMapping
     public ApiResult<CharacterResponseDto> createCharacter(@PathVariable Long productId, @PathVariable Long teamId,
                                                            @RequestBody @Valid CharacterRequestDto characterRequestDto,
-                                                           @AuthenticationPrincipal PrincipalDetails principalDetails){
-        Character character = characterService.createCharacter(CharacterRequestDto.from(characterRequestDto), teamId, principalDetails.getUser());
+                                                           @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Character character = characterService.createCharacter(CharacterRequestDto.from(characterRequestDto), productId, teamId, principalDetails.getUser());
         return ApiResult.OK(CharacterResponseDto.of(character));
     }
 }
