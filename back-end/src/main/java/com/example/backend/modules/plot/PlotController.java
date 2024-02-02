e3b950770d8c07252f794ec50c18923dae2fd6f7
심지연 <sim.yyeon@gmail.com>
2024-02-02 15:00:04 +0900

    Feat: add 생성, 수정, 삭제 로직 구현

--- back-end/src/main/java/com/example/backend/modules/plot/PlotController.java
+++ back-end/src/main/java/com/example/backend/modules/plot/PlotController.java
@@ -1,10 +1,12 @@
 package com.example.backend.modules.plot;
 
 import com.example.backend.modules.api.ApiResult;
+import com.example.backend.modules.auth.principal.PrincipalDetails;
 import com.example.backend.modules.product.ProductService;
 import com.example.backend.modules.team.TeamService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,11 +18,31 @@ public class PlotController {
     private final ProductService productService;
 
     @PostMapping("/")
-    public ApiResult<Plot> createPlot(@RequestBody @Valid PlotRequestDto plotRequestDto,
+    public ApiResult<PlotResponseDto> createPlot(@RequestBody @Valid PlotRequestDto plotRequestDto,
                                       @PathVariable("teamId") Long teamId,
-                                      @PathVariable("productId") Long productId) {
-        Plot plot = null;
-        return ApiResult.OK(plot);
+                                      @PathVariable("productId") Long productId,
+                                      @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        Plot plot = plotService.createPlot(principalDetails.getUser(), teamId, productId, PlotRequestDto.of(plotRequestDto));
+        return ApiResult.OK(PlotResponseDto.from(plot));
     }
+
+    @PatchMapping("/{plotId}")
+    public ApiResult<PlotResponseDto> updatePlot(@RequestBody @Valid PlotRequestDto plotRequestDto,
+                                                 @PathVariable("teamId") Long teamId,
+                                                 @PathVariable("productId") Long productId,
+                                                 @AuthenticationPrincipal PrincipalDetails principalDetails){
+        Plot plot = plotService.updatePlot(principalDetails.getUser(), teamId, productId, PlotRequestDto.of(plotRequestDto));
+        return ApiResult.OK(PlotResponseDto.from(plot));
+    }
+
+    @DeleteMapping("/{plotId}")
+    public ApiResult<PlotResponseDto> deletePlot(@PathVariable("plotId") Long plotId,
+                                                 @PathVariable("teamId") Long teamId,
+                                                 @PathVariable("productId") Long productId,
+                                                 @AuthenticationPrincipal PrincipalDetails principalDetails){
+        plotService.deletePlot(principalDetails.getUser(),teamId,productId,plotId);
+        return ApiResult.OK(null);
+    }
+
 
 }
