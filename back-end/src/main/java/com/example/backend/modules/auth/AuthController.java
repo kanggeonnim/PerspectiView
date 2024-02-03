93f80fbfb1e5dae0c92f081b3d7e9d08d6173acb
kanggeonnim <gun4329@gmail.com>
2024-02-03 19:29:08 +0900

    Refactor: change AuthController return value

--- back-end/src/main/java/com/example/backend/modules/auth/AuthController.java
+++ back-end/src/main/java/com/example/backend/modules/auth/AuthController.java
@@ -1,15 +1,15 @@
 package com.example.backend.modules.auth;
 
+import com.example.backend.modules.api.ApiResult;
 import com.example.backend.modules.auth.StatusResponseDto;
 import com.example.backend.modules.auth.TokenResponseStatus;
 import com.example.backend.modules.auth.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -19,22 +19,22 @@ public class AuthController {
     private final RefreshTokenService tokenService;
 
     @PostMapping("/token/logout")
-    public ResponseEntity<StatusResponseDto> logout(@RequestHeader(value = "Authorization") final String accessToken) {
+    public ApiResult<StatusResponseDto> logout(@RequestHeader(value = "Authorization") final String accessToken) {
         // 엑세스 토큰으로 현재 Redis 정보 삭제
         tokenService.removeRefreshToken(accessToken);
 
-        return ResponseEntity.ok(StatusResponseDto.addStatus(200));
+        return ApiResult.OK(null);
     }
 
     @PostMapping("/token/refresh")
-    public ResponseEntity<TokenResponseStatus> refresh(@RequestHeader("Authorization") final String accessToken) {
+    public ApiResult<?> refresh(@RequestHeader("Authorization") final String refreshToken) {
 
-        String newAccessToken = tokenService.republishAccessToken(accessToken);
+        String newAccessToken = tokenService.republishAccessToken(refreshToken);
         if (StringUtils.hasText(newAccessToken)) {
-            return ResponseEntity.ok(TokenResponseStatus.addStatus(200, newAccessToken));
+            return ApiResult.OK(TokenResponseStatus.addStatus(200, newAccessToken));
         }
 
-        return ResponseEntity.badRequest().body(TokenResponseStatus.addStatus(400, null));
+        return ApiResult.ERROR("유효한 토큰이 아닙니다.", HttpStatus.NON_AUTHORITATIVE_INFORMATION);
     }
 
 }
