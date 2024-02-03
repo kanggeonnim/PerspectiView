8557ca0d4055db21299cad2e86f7f62549eb9150
jack <yjk9805@naver.com>
2024-02-03 15:37:39 +0900

    Feat: implement logic put s3

--- back-end/src/main/java/com/example/backend/infra/s3/S3Uploader.java
+++ back-end/src/main/java/com/example/backend/infra/s3/S3Uploader.java
@@ -1,9 +1,13 @@
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @Component
 @Slf4j
@@ -27,4 +31,12 @@ public class S3Uploader {
         return false;
     }
 
+    // s3 버킷 put
+    private String putS3(File uploadFile, String fileName) {
+        s3.putObject(
+                new PutObjectRequest(bucketName, fileName, uploadFile)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한 업로드
+        );
+        return s3URL+"/"+fileName;
+    }
 }
