eb3021d38561197eefe25bb3392e55aec3e9b7e1
jack <yjk9805@naver.com>
2024-02-03 15:39:34 +0900

    Feat: implement logic put s3 and return url

--- back-end/src/main/java/com/example/backend/infra/s3/S3Uploader.java
+++ back-end/src/main/java/com/example/backend/infra/s3/S3Uploader.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.UUID;
 
 @Component
 @Slf4j
@@ -22,9 +23,20 @@ public class S3Uploader {
     private final String folderName = "images";
 
 
+    // S3 업로드 및 url 반환
+    private String putS3AndReturnURL(File uploadFile) {
+        String uuid = UUID.randomUUID().toString();
+        String fileName = folderName + "/" + uuid + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+
+        removeNewFile(uploadFile);
+
+        return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
+    }
+
     // null 체크, 이미지 타입 체크
-    public boolean verify(MultipartFile multipartFile){
-        if(multipartFile != null && multipartFile.getSize() > 0 && multipartFile.getOriginalFilename() != null){
+    public boolean verify(MultipartFile multipartFile) {
+        if (multipartFile != null && multipartFile.getSize() > 0 && multipartFile.getOriginalFilename() != null) {
             String contentType = multipartFile.getContentType();
             return (contentType != null) && contentType.toLowerCase().startsWith("image");
         }
@@ -35,17 +47,17 @@ public class S3Uploader {
     private String putS3(File uploadFile, String fileName) {
         s3.putObject(
                 new PutObjectRequest(bucketName, fileName, uploadFile)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한 업로드
+                        .withCannedAcl(CannedAccessControlList.PublicRead)    // PublicRead 권한 업로드
         );
-        return s3URL+"/"+fileName;
+        return s3URL + "/" + fileName;
     }
 
 
     // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성)
     private void removeNewFile(File targetFile) {
-        if(targetFile.delete()) {
+        if (targetFile.delete()) {
             log.info("파일이 삭제되었습니다.");
-        }else {
+        } else {
             log.info("파일이 삭제되지 못했습니다.");
         }
     }
