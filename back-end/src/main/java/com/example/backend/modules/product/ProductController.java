3a2b09093f38ee6bcd00c1f1241542eb98b7da46
kanggeonnim <gun4329@gmail.com>
2024-02-04 04:21:08 +0900

    Feat: add ProductRelation and plots

--- back-end/src/main/java/com/example/backend/modules/product/ProductController.java
+++ back-end/src/main/java/com/example/backend/modules/product/ProductController.java
@@ -1,15 +1,11 @@
 package com.example.backend.modules.product;
 
-import com.example.backend.modules.account.User;
 import com.example.backend.modules.api.ApiResult;
 import com.example.backend.modules.auth.principal.PrincipalDetails;
 import com.example.backend.modules.genre.Genre;
-import com.example.backend.modules.team.Team;
-import com.example.backend.modules.team.TeamService;
+import com.example.backend.modules.plot.Plot;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,7 +25,7 @@ public class ProductController {
 
         Product product = productService.createTeamProduct(principalDetails.getUser(), teamId, productRequestDto.of(productRequestDto),productRequestDto.getGenres());
         List<Genre> genres = productService.findGenreList(product.getProductGenres());
-        return ApiResult.OK(ProductResponseDto.from(product,genres));
+        return ApiResult.OK(ProductResponseDto.from(product,genres, null, null));
     }
 
     @GetMapping("/{productId}")
@@ -37,7 +33,9 @@ public class ProductController {
                                                     @AuthenticationPrincipal PrincipalDetails principalDetails) {
         Product product = productService.findByProductId(principalDetails.getUser(), teamId, productId);
         List<Genre> genres = productService.findGenreList(product.getProductGenres());
-        return ApiResult.OK(ProductResponseDto.from(product,genres));
+        List<ProductRelation> productRelations = productService.findProductRelations(productId);
+        List<Plot> plots = productService.findPlots(productId);
+        return ApiResult.OK(ProductResponseDto.from(product, genres, productRelations, plots));
     }
 
     @PatchMapping("/{productId}")
@@ -48,7 +46,9 @@ public class ProductController {
         productRequestDto.setProductId(productId);
         Product product = productService.updateProduct(principalDetails.getUser(), teamId, productRequestDto.of(productRequestDto),productRequestDto.getGenres());
         List<Genre> genres = productService.findGenreList(product.getProductGenres());
-        return ApiResult.OK(ProductResponseDto.from(product,genres));
+        List<ProductRelation> productRelations = productService.findProductRelations(productId);
+        List<Plot> plots = productService.findPlots(productId);
+        return ApiResult.OK(ProductResponseDto.from(product,genres, productRelations, plots));
     }
 
     @PatchMapping("/title/{productId}")
@@ -58,8 +58,7 @@ public class ProductController {
                                                                 @AuthenticationPrincipal PrincipalDetails principalDetails) {
         productRequestDto.setProductId(productId);
         Product product = productService.updateProductTitle(principalDetails.getUser(), teamId, productRequestDto.of(productRequestDto));
-        List<Genre> genres = productService.findGenreList(product.getProductGenres());
-        return ApiResult.OK(ProductResponseDto.from(product,genres));
+        return ApiResult.OK(ProductResponseDto.from(product,null, null, null));
     }
 
     @DeleteMapping("/{productId}")
