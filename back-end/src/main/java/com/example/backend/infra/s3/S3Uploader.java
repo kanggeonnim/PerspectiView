9b9039c8581887898b175b47802cb2a097a30bff
jack <yjk9805@naver.com>
2024-02-03 15:40:40 +0900

    Feat: implement logic convert MultipartFile to File

--- back-end/src/main/java/com/example/backend/infra/s3/S3Uploader.java
+++ back-end/src/main/java/com/example/backend/infra/s3/S3Uploader.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
@@ -23,6 +26,21 @@ public class S3Uploader {
     private final String folderName = "images";
 
 
+
+    // MultipartFile -> File convert
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        if(verify(file)){
+            File convertFile = new File(file.getOriginalFilename());
+            if(convertFile.createNewFile()) {
+                try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                    fos.write(file.getBytes());
+                }
+                return Optional.of(convertFile);
+            }
+            return Optional.empty();
+        }
+        throw new RuntimeException(); // TODO Exception
+    }
     // S3 업로드 및 url 반환
     private String putS3AndReturnURL(File uploadFile) {
         String uuid = UUID.randomUUID().toString();
