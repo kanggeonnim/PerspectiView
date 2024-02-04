e13b74d92ba8c1f56302903de8acf230df2d73df
kanggeonnim <gun4329@gmail.com>
2024-02-04 12:19:45 +0900

    Feat: add ProductRelations, plots

--- back-end/src/main/java/com/example/backend/modules/product/ProductService.java
+++ back-end/src/main/java/com/example/backend/modules/product/ProductService.java
@@ -4,9 +4,11 @@ import com.example.backend.modules.account.User;
 import com.example.backend.modules.account.UserService;
 import com.example.backend.modules.genre.Genre;
 import com.example.backend.modules.genre.GenreRepository;
+import com.example.backend.modules.plot.Plot;
 import com.example.backend.modules.team.Team;
 import com.example.backend.modules.team.TeamService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -169,11 +171,19 @@ public class ProductService {
         return genres;
     }
 
-    /** todo
-     * 팀 인물 관계 조회
+    /**
+     * 작품 인물 관계 조회
      */
+    public List<ProductRelation> findProductRelations(Long productId){
+        Product product = productRepository.findById(productId).orElseThrow(() -> new RuntimeException());
+        return product.getProductRelations().stream().toList();
 
-    /** todo
-     * 팀 플롯 전체 조회
+    }
+    /**
+     * 작품 플롯 전체 조회
      */
+    public List<Plot> findPlots(Long productId){
+        Product product = productRepository.findById(productId).orElseThrow(() -> new RuntimeException());
+       return product.getPlots().stream().toList();
+    }
 }
