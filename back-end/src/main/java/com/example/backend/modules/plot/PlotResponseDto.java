70f9c2cf1c88a49223ddff5eaf92f422b0285f20
심지연 <sim.yyeon@gmail.com>
2024-02-04 12:15:38 +0900

    Feat: add storylist

--- back-end/src/main/java/com/example/backend/modules/plot/PlotResponseDto.java
+++ back-end/src/main/java/com/example/backend/modules/plot/PlotResponseDto.java
@@ -1,21 +1,25 @@
 package com.example.backend.modules.plot;
 
+import com.example.backend.modules.story.Story;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
 public class PlotResponseDto {
-    Long plotId;
-    String plotName;
-    String plotColor;
-    //TODO STORY List 가져오기
+    private Long plotId;
+    private String plotName;
+    private String plotColor;
+    private List<Story> storyList;
 
     public static PlotResponseDto from(Plot plot){
         return PlotResponseDto.builder()
                 .plotId(plot.getId())
                 .plotName(plot.getName())
                 .plotColor(plot.getColor())
+                .storyList(plot.getStories())
                 .build();
     }
 }
