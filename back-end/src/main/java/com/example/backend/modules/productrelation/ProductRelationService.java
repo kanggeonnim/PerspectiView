3d23420c1c8369b8c51b36884648e00013f1b70f
kanggeonnim <gun4329@gmail.com>
2024-02-05 22:59:49 +0900

    Feat: add ProductRelationService

---
+++ back-end/src/main/java/com/example/backend/modules/productrelation/ProductRelationService.java
@@ -0,0 +1,54 @@
+package com.example.backend.modules.productrelation;
+
+import com.example.backend.modules.account.User;
+import com.example.backend.modules.product.Product;
+import com.example.backend.modules.product.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ProductRelationService {
+    private final ProductRelationRepository productRelationRepository;
+    private final ProductService productService;
+    /**
+     * 작품 인물 관계 생성
+     * @param productRelation
+     * @return
+     */
+    @Transactional
+    public ProductRelation createProductRelation(ProductRelation productRelation){
+        ProductRelation newProductRelation = productRelationRepository.save(productRelation);
+        return newProductRelation;
+    }
+
+    /**
+     * 전체 인물관계 조회
+     */
+    public List<ProductRelation> findAllProductRelation(User user, Long teamId, Long productId){
+        Product product = productService.findByProductId(user, teamId, productId);
+        List<ProductRelation> productRelations = productRelationRepository.findAllByProduct(product);
+        return productRelations;
+    }
+
+    /**
+     * 인물관계 수정
+     */
+    @Transactional
+    public ProductRelation updateProductRelation(Long productRelationId, ProductRelation productRelation){
+        ProductRelation findProductRelation = productRelationRepository.findById(productRelationId)
+                .orElseThrow(() -> new RuntimeException());
+        findProductRelation.updateProductRelation(productRelation);
+        return findProductRelation;
+    }
+
+    @Transactional
+    public void deleteProductRelation(Long productRelationId){
+        productRelationRepository.deleteById(productRelationId);
+    }
+}
