0566662121183655a7352cf6b9ee569720f06bc1
jack <yjk9805@naver.com>
2024-02-05 14:58:04 +0900

    Feat: add Exception Handler

--- back-end/src/main/java/com/example/backend/modules/api/MainControllerAdvice.java
+++ back-end/src/main/java/com/example/backend/modules/api/MainControllerAdvice.java
@@ -1,5 +1,9 @@
 package com.example.backend.modules.api;
 
+import com.example.backend.modules.exception.BadRequestException;
+import com.example.backend.modules.exception.ForbiddenException;
+import com.example.backend.modules.exception.NotFoundException;
+import com.example.backend.modules.exception.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,4 +16,23 @@ public class MainControllerAdvice {
         return ApiResult.ERROR(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(NotFoundException.class)
+    public ApiResult<?> handleNotFoundException(NotFoundException e){
+        return ApiResult.ERROR(e.getMessage(), e.getHttpStatus());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ApiResult<?> handleBadException(BadRequestException e){
+        return ApiResult.ERROR(e.getMessage(), e.getHttpStatus());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ApiResult<?> handleUnauthorizedException(UnauthorizedException e){
+        return ApiResult.ERROR(e.getMessage(), e.getHttpStatus());
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ApiResult<?> handleForbiddenException(ForbiddenException e){
+        return ApiResult.ERROR(e.getMessage(), e.getHttpStatus());
+    }
 }
