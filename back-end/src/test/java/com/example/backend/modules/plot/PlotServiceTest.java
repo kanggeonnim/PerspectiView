37ef8c9a9e0d75b1cb1b3d0cb622be6611a76a01
심지연 <sim.yyeon@gmail.com>
2024-02-05 17:47:41 +0900

    Test: add plot test

---
+++ back-end/src/test/java/com/example/backend/modules/plot/PlotServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.backend.modules.plot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlotServiceTest {
+
+    @Test
+    public void createPlot() {
+    }
+
+    @Test
+    public void findByProductId() {
+    }
+
+    @Test
+    public void updatePlot() {
+    }
+
+    @Test
+    public void deletePlot() {
+    }
+}
