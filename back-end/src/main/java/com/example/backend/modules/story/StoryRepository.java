706187a79de09be19eda3c6ee74d0c6bd8a08162
SIMJIYEON <sim.yyeon@gmail.com>
2024-02-06 01:22:08 +0900

    Feat: add findByPlot

--- back-end/src/main/java/com/example/backend/modules/story/StoryRepository.java
+++ back-end/src/main/java/com/example/backend/modules/story/StoryRepository.java
@@ -1,14 +1,19 @@
 package com.example.backend.modules.story;
 
+import com.example.backend.modules.plot.Plot;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StoryRepository extends JpaRepository<Story,Long> {
     @Modifying
     @Query("UPDATE Story s " +
             "SET s.positionX = s.positionX + 1 " +
             "WHERE s.plot.id = :plotId AND s.positionX >= :positionX")
-    void updatePositionX(@Param("plotId") Long plotId, @Param("PositionX") int positionX);
+    void updatePositionX(@Param("plotId") Long plotId, @Param("positionX") int positionX);
+
+    List<Story> findByPlot(Plot plot);
 }
