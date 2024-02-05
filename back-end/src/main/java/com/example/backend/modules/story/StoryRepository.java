dd0c2f6141ee92f21a065df31c3913ce3c5a497c
심지연 <sim.yyeon@gmail.com>
2024-02-05 14:54:18 +0900

    Comment: story 위치 조정 쿼리 todo

--- back-end/src/main/java/com/example/backend/modules/story/StoryRepository.java
+++ back-end/src/main/java/com/example/backend/modules/story/StoryRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface StoryRepository extends JpaRepository<Story,Long> {
+    //TODO PLOTID = plotId and POSITIONX 보다 큰 POSTIONX 로만 POSITINOX +1 해줘야함
     @Modifying
     @Query("UPDATE Story s " +
             "SET s.positionX = :positionX " +
