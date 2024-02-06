6e52d0fc271a3d7a0e881116f029784cc74701ab
심지연 <sim.yyeon@gmail.com>
2024-02-06 17:40:52 +0900

    Feat: add storyList

--- back-end/src/main/java/com/example/backend/modules/foreshadowing/ForeShadowingResponseDto.java
+++ back-end/src/main/java/com/example/backend/modules/foreshadowing/ForeShadowingResponseDto.java
@@ -1,7 +1,10 @@
 package com.example.backend.modules.foreshadowing;
 
+import com.example.backend.modules.story.Story;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -10,13 +13,16 @@ public class ForeShadowingResponseDto {
     String foreShadowingName;
     String foreShadowingContent;
     boolean foreShadowingClose;
+    List<Long> storyList;
 
+//    public static ForeShadowingResponseDto from(ForeShadowing foreShadowing,List<Long> storyList){
     public static ForeShadowingResponseDto from(ForeShadowing foreShadowing){
         return ForeShadowingResponseDto.builder()
                 .foreShadowingId(foreShadowing.getId())
                 .foreShadowingName(foreShadowing.getFShadowName())
                 .foreShadowingContent(foreShadowing.getFShadowContent())
                 .foreShadowingClose(foreShadowing.isFShadowClose())
+//                .storyList(storyList)
                 .build();
     }
 }
