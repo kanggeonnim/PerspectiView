caa39de2c526e4b2a0bf3e7bc38e45d7e0dbf90a
jack <yjk9805@naver.com>
2024-02-06 14:40:14 +0900

    Fix: modify db config

--- back-end/src/main/java/com/example/backend/infra/security/ProductCheckVoter.java
+++ back-end/src/main/java/com/example/backend/infra/security/ProductCheckVoter.java
@@ -1,14 +1,12 @@
 package com.example.backend.infra.security;
 
 import com.example.backend.modules.auth.principal.PrincipalDetails;
-import com.example.backend.modules.product.Product;
 import com.example.backend.modules.product.ProductService;
 import com.example.backend.modules.team.Team;
 import com.example.backend.modules.team.TeamService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -32,13 +30,12 @@ public class ProductCheckVoter implements AccessDecisionVoter<FilterInvocation> 
     private final Function<String, Long> productIdExtractor;
 
     private final TeamService teamService;
-
     private final ProductService productService;
 
     @Autowired
     public ProductCheckVoter(TeamService teamService, ProductService productService) {
 //        final String regex = "^/api/team/(\\d+)/post/.*$";
-        final String regex = "^/api/team/(\\d+)/product/(\\d+)";
+        final String regex = "^/api/team/(\\d+)/product/(\\d+).*$";
         final Pattern pattern = Pattern.compile(regex);
         RequestMatcher requiresAuthorizationRequestMatcher = new RegexRequestMatcher(pattern.pattern(), null);
 
@@ -57,7 +54,6 @@ public class ProductCheckVoter implements AccessDecisionVoter<FilterInvocation> 
         this.requiresAuthorizationRequestMatcher = requiresAuthorizationRequestMatcher;
         this.teamService = teamService;
         this.productService = productService;
-
     }
 
     @Override
@@ -83,6 +79,7 @@ public class ProductCheckVoter implements AccessDecisionVoter<FilterInvocation> 
         }
 
 
+
         // 접근 유저가 팀의 매니저인지 체크
         PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
         Long targetTeamId = obtainTeamId(request);
@@ -90,11 +87,14 @@ public class ProductCheckVoter implements AccessDecisionVoter<FilterInvocation> 
         Team team = teamService.getTeam(targetTeamId);
 
 
-        if(request.getMethod().equals(HttpMethod.GET.name())){
-            if(team.ifMember(principal.getUser()) || team.ifManager(principal.getUser())) return ACCESS_GRANTED;
+
+        // GET 요청 시 팀 멤버면 통과
+        if(request.getMethod().equals(HttpMethod.GET)){
+            if(team.ifManager(principal.getUser()) || team.ifMember(principal.getUser())){
+                return ACCESS_GRANTED;
+            }
         }
 
-        // 프로덕트 체크 -> 해당 팀인지
         // 매니저가 아니면 예외
         if(!team.ifManager(principal.getUser())) return ACCESS_DENIED;
 
@@ -104,6 +104,7 @@ public class ProductCheckVoter implements AccessDecisionVoter<FilterInvocation> 
     private Long obtainTeamId(HttpServletRequest request) {
         return teamIdExtractor.apply(request.getRequestURI());
     }
+
     private Long obtainProductId(HttpServletRequest request) {
         return productIdExtractor.apply(request.getRequestURI());
     }
