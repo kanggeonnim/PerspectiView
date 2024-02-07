aa9b1bcc7e171da096819d9ec5834030e8851dfe
jack <yjk9805@naver.com>
2024-02-07 23:05:07 +0900

    Feat: modify cors config

--- back-end/src/main/java/com/example/backend/infra/config/SecurityConfig.java
+++ back-end/src/main/java/com/example/backend/infra/config/SecurityConfig.java
@@ -18,11 +18,9 @@ import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration // IoC 빈(bean)을 등록
@@ -35,20 +33,19 @@ public class SecurityConfig {
 	private final JwtAuthorizationFilter jwtAuthorizationFilter;
 	private final JwtExceptionFilter jwtExceptionFilter;
 	private final TeamCheckVoter teamCheckVoter;
-	private final ProductCheckVoter productCheckVoter;
 
 	@Bean
 	public BCryptPasswordEncoder encodePwd() {
 		return new BCryptPasswordEncoder();
 	}
 
+
 	@Bean
 	public AccessDecisionManager accessDecisionManager() {
 		List<AccessDecisionVoter<?>> decisionVoters = new ArrayList<>();
 		decisionVoters.add(new WebExpressionVoter());
 		// voter 목록에 teamCheckVoter 추가
-//		decisionVoters.add(teamCheckVoter);
-//		decisionVoters.add(productCheckVoter);
+		decisionVoters.add(teamCheckVoter);
 		// 모든 voter 승인 시 허가
 		return new UnanimousBased(decisionVoters);
 	}
@@ -58,25 +55,22 @@ public class SecurityConfig {
 		http
 				.csrf(AbstractHttpConfigurer::disable)
 				.httpBasic(AbstractHttpConfigurer::disable)
-//				.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
-				.cors(AbstractHttpConfigurer::disable)
-//				.headers((headers)->
-//						headers.contentTypeOptions(contentTypeOptionsConfig ->
-//								headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)))
+				.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+				.headers((headers)->
+						headers.contentTypeOptions(contentTypeOptionsConfig ->
+								headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)))
 				.formLogin(AbstractHttpConfigurer::disable) // form 로그인 비활성화
 				.sessionManagement(sessionManagement->sessionManagement
 						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeRequests(authorize ->
-
 						// accessDecisionManager
 						authorize.accessDecisionManager(accessDecisionManager())
 								.requestMatchers("/v3/**", "/swagger-ui/**").permitAll()
 						.requestMatchers("/user/**").authenticated()
-								.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-//						 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or
-//						 hasRole('ROLE_USER')")
-//						 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and
-//						 hasRole('ROLE_USER')")
+						// .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or
+						// hasRole('ROLE_USER')")
+						// .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and
+						// hasRole('ROLE_USER')")
 						.requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
 										.requestMatchers("/genre/**").permitAll()
 										.requestMatchers("/category/**").permitAll()
@@ -87,8 +81,8 @@ public class SecurityConfig {
 								.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(principalOauth2UserService))
 								.successHandler(jwtSuccessHandler)
 								.failureHandler(myAuthenticationFailureHandler))
-				.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
-//				.addFilterBefore(jwtExceptionFilter, JwtAuthorizationFilter.class);
+				.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+				.addFilterBefore(jwtExceptionFilter, JwtAuthorizationFilter.class);
 
 
 		return http.build();
@@ -98,13 +92,8 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.addAllowedOrigin("http://localhost:5173");
-
-//		configuration.setAllowedOrigins(Arrays.asList("*"));
-
-		configuration.addAllowedHeader("*");
 		configuration.addExposedHeader("Authorization");
-		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-//		configuration.addAllowedMethod("*"); // 모든 HTTP 메소드 허용
+		configuration.addAllowedMethod("*"); // 모든 HTTP 메소드 허용
 		configuration.setAllowCredentials(true);
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
