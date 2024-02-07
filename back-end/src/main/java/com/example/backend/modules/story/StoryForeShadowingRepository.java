bc1d7b772b8bce19fdf9578cee690a63b7dd1f18
심지연 <sim.yyeon@gmail.com>
2024-02-07 23:25:00 +0900

    Feat: add entitygraph to StoryForeShadowing

--- back-end/src/main/java/com/example/backend/modules/story/StoryForeShadowingRepository.java
+++ back-end/src/main/java/com/example/backend/modules/story/StoryForeShadowingRepository.java
@@ -1,6 +1,12 @@
 package com.example.backend.modules.story;
 
+import com.example.backend.modules.foreshadowing.ForeShadowing;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StoryForeShadowingRepository extends JpaRepository<StoryForeShadowing, Long> {
+    @EntityGraph(attributePaths = {"story"})
+    List<StoryForeShadowing> findWithStoryByForeShadowing(ForeShadowing foreShadowing);
 }
