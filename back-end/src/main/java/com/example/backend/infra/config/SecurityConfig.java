ee7a41531f03db80ec1ded1e76459160e5fa536d
jack <yjk9805@naver.com>
2024-02-07 17:10:53 +0900

    Feat: add cors

--- back-end/src/main/java/com/example/backend/infra/config/SecurityConfig.java
+++ back-end/src/main/java/com/example/backend/infra/config/SecurityConfig.java
@@ -93,14 +93,14 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-//		configuration.addAllowedOrigin("http://localhost:5173");
+		configuration.addAllowedOrigin("http://localhost:5173");
 
-		configuration.setAllowedOrigins(Arrays.asList("*"));
+//		configuration.setAllowedOrigins(Arrays.asList("*"));
 
 
 		configuration.addExposedHeader("Authorization");
 		configuration.addAllowedMethod("*"); // 모든 HTTP 메소드 허용
-//		configuration.setAllowCredentials(true);
+		configuration.setAllowCredentials(true);
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
