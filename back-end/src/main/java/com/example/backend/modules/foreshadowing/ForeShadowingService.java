a49e9b510b03f55792c48341793d45cac347ea7b
심지연 <sim.yyeon@gmail.com>
2024-02-07 23:22:13 +0900

    Feat: add 복선스토리리스트

--- back-end/src/main/java/com/example/backend/modules/foreshadowing/ForeShadowingService.java
+++ back-end/src/main/java/com/example/backend/modules/foreshadowing/ForeShadowingService.java
@@ -1,5 +1,8 @@
 package com.example.backend.modules.foreshadowing;
 
+import com.example.backend.modules.story.Story;
+import com.example.backend.modules.story.StoryForeShadowing;
+import com.example.backend.modules.story.StoryForeShadowingRepository;
 import com.example.backend.modules.user.User;
 import com.example.backend.modules.product.Product;
 import com.example.backend.modules.product.ProductService;
@@ -7,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +19,8 @@ public class ForeShadowingService {
     private final ForeShadowingRepository foreShadowingRepository;
 
     private final ProductService productService;
+
+    private final StoryForeShadowingRepository storyForeShadowingRepository;
 
     /**
      * 복선 생성
@@ -70,7 +75,7 @@ public class ForeShadowingService {
         }
 
         ForeShadowing findForeShadowing = foreShadowingRepository.findById(foreShadowing.getId()).orElseThrow(() -> new RuntimeException());
-        findForeShadowing.updateForeShadowing(findForeShadowing.getFShadowName(), findForeShadowing.getFShadowContent(), findForeShadowing.isFShadowClose());
+        findForeShadowing.updateForeShadowing(findForeShadowing.getFShadowName(), findForeShadowing.getFShadowContent(), findForeShadowing.getFShadowClose());
         return foreShadowing;
     }
 
@@ -90,4 +95,35 @@ public class ForeShadowingService {
         }
         foreShadowingRepository.deleteById(fShadowingId);
     }
+
+    /**
+     * 복선의 스토리 리스트 출력
+     */
+    public List<FshadowStoryIdDto> findStories(ForeShadowing foreShadowing) {
+        //복선으로 중간 테이블 리스트 받아오기
+        List<StoryForeShadowing> sfs = storyForeShadowingRepository.findByForeShadowing(foreShadowing);
+        List<FshadowStoryIdDto> storyids = new ArrayList<>();
+        //중간테이블로 스토리 정보 얻기
+        for (StoryForeShadowing sf : sfs) {
+            //storyforeshadowing에 Entitygraph로 join 해서 story정보 담겨있음
+            storyids.add(FshadowStoryIdDto.builder()
+                    .storyId(sf.getStory().getId())
+                    .build());
+        }
+        return storyids;
+    }
+
+    /**
+     * 복선 스토리에 추가
+     */
+
+
+    /**
+     * 복선 스토리에서 삭제
+     */
+
+    /**
+     * 복선 회수
+     */
+
 }
