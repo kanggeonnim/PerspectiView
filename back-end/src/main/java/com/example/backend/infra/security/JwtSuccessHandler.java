0cb9a7c6c8666d4073a1679376b4ee7209d8f687
kanggeonnim <gun4329@gmail.com>
2024-02-08 20:57:30 +0900

    Feat: add JwtSuccessHandler redirect to Cookie

--- back-end/src/main/java/com/example/backend/infra/security/JwtSuccessHandler.java
+++ back-end/src/main/java/com/example/backend/infra/security/JwtSuccessHandler.java
@@ -6,10 +6,13 @@ import com.example.backend.modules.auth.TokenResponseDto;
 import com.example.backend.modules.auth.principal.PrincipalDetails;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
@@ -18,7 +21,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
 @Component
 @RequiredArgsConstructor
@@ -41,10 +46,30 @@ public class JwtSuccessHandler implements AuthenticationSuccessHandler {
                         objectMapper.writeValueAsString(principalDetails.getUser().getAuthorities())
                 );
 
-        response.sendRedirect(UriComponentsBuilder.fromUriString("http://localhost:5173/app/workspace")
+        // 쿠키 설정
+        ResponseCookie accessCookie = ResponseCookie.from("accessToken", URLEncoder.encode(token.getAccessToken(), "UTF-8"))
+                .httpOnly(true)
+                .secure(true)
+                .path("/")      // path
+                .maxAge(Duration.ofDays(1))
+                .sameSite("None")  // sameSite
+                .build();
+
+        ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", URLEncoder.encode(token.getRefreshToken(), "UTF-8"))
+                .httpOnly(true)
+                .secure(true)
+                .path("/")      // path
+                .maxAge(Duration.ofDays(1))
+                .sameSite("None")  // sameSite
+                .build();
+
+        response.setHeader(HttpHeaders.SET_COOKIE, accessCookie.toString());
+        response.setHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
+
+        response.sendRedirect(UriComponentsBuilder.fromUriString("http://localhost:8081/api")
 //        response.sendRedirect(UriComponentsBuilder.fromUriString("https://i10b310.p.ssafy.io/app/workspace")
-                .queryParam("accessToken", token.getAccessToken())
-                .queryParam("refreshToken", token.getRefreshToken())
+//                .queryParam("accessToken", token.getAccessToken())
+//                .queryParam("refreshToken", token.getRefreshToken())
                 .build()
                 .encode(StandardCharsets.UTF_8)
                 .toUriString());
