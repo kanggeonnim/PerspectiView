6960c5821b9fbd581679e0afba16613d003ccaae
심지연 <sim.yyeon@gmail.com>
2024-02-08 20:00:53 +0900

    Feat: add findByTeamId

--- back-end/src/main/java/com/example/backend/modules/product/ProductRepository.java
+++ back-end/src/main/java/com/example/backend/modules/product/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.example.backend.modules.product;
 
+import com.example.backend.modules.team.Team;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @EntityGraph(attributePaths = {"team"})
     Optional<Product> findWithTeamById(Long id);
+
+
+    List<Product> findByTeamId(Long teamId);
 }
