1f5a7a997bc6d9b7c48a12560005312e6ef63581
우재하 <jaeha6049@naver.com>
2024-02-09 23:00:56 +0900

    Fix: useProductQueryModule.jsx 개선

--- front-end/src/hook/useProductQueryModule.js
+++ front-end/src/hook/useProductQueryModule.js
@@ -1,7 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { privateApi } from "@/util/api";
 
 const useProductQueryModule = (teamId) => {
+  const queryClient = useQueryClient();
+
+
   const { data: productData, isSuccess: getProductIsSuccess } = useQuery({
     queryKey: ["product", teamId],
     queryFn: async () => {
@@ -11,7 +14,20 @@ const useProductQueryModule = (teamId) => {
     },
   });
 
-  return { productData, getProductIsSuccess };
+  const { mutate: createProduct } = useMutation({
+    mutationFn: async (newData) => {
+      const response = await privateApi.post(`/team/${teamId}/product`, newData);
+      console.log(response);
+      return response.data.response;
+    },
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["plotList"] });
+    },
+  });
+
+
+  return { productData, getProductIsSuccess, createProduct };
 };
 
 export default useProductQueryModule;
