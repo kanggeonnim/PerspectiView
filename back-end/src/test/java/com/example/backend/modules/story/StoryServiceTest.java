a105308c3c20013c8aba9a044a4bba0dddb163cd
jack <yjk9805@naver.com>
2024-02-09 13:32:58 +0900

    Fix: modify character test logic

--- back-end/src/test/java/com/example/backend/modules/story/StoryServiceTest.java
+++ back-end/src/test/java/com/example/backend/modules/story/StoryServiceTest.java
@@ -164,8 +164,8 @@ class StoryServiceTest {
                 .product(product)
                 .characterName("toCharacter")
                 .build();
-        characterService.createCharacter(fromCharacter, product.getId(), user);
-        characterService.createCharacter(toCharacter, product.getId(), user);
+        characterService.createCharacter(fromCharacter, team.getId(), user);
+        characterService.createCharacter(toCharacter, team.getId(), user);
 
         foreShadowing = ForeShadowing.builder()
                 .product(product)
