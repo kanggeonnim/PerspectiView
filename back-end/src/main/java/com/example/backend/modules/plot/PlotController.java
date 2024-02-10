358e47017397683a2c76eb9b8b361b79e4342c55
심지연 <sim.yyeon@gmail.com>
2024-02-11 02:55:09 +0900

    Feat: modify createPlot ResponseDto

--- back-end/src/main/java/com/example/backend/modules/plot/PlotController.java
+++ back-end/src/main/java/com/example/backend/modules/plot/PlotController.java
@@ -45,11 +45,15 @@ public class PlotController {
     }
 
     @PostMapping
-    public ApiResult<PlotResponseDto> createPlot(@RequestBody @Valid PlotRequestDto plotRequestDto,
+    public ApiResult<List<PlotPreviewResponseDto>> createPlot(@RequestBody @Valid PlotRequestDto plotRequestDto,
                                                  @PathVariable("productId") Long productId) {
         log.info("== plot create controller===");
         Plot plot = plotService.createPlot(productId, PlotRequestDto.from(plotRequestDto));
-        return ApiResult.OK(PlotResponseDto.of(plot));
+        //plot 생성 하고 나서도 작품의 플롯 리스트 전체 return
+        List<Plot> plots = plotService.findByProductId(productId);
+        //plot List 정렬
+        plots.sort(Comparator.comparing(Plot::getId));
+        return ApiResult.OK(plots.stream().map(PlotPreviewResponseDto::of).collect(Collectors.toList()));
     }
 
     @PutMapping("/{plotId}")
