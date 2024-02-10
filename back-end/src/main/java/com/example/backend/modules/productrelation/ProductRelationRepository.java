e01908d3e99c39271f6a88ec45df108be973adc5
kanggeonnim <gun4329@gmail.com>
2024-02-11 03:15:01 +0900

    Fix: update ProductRelation insert Character

--- back-end/src/main/java/com/example/backend/modules/productrelation/ProductRelationRepository.java
+++ back-end/src/main/java/com/example/backend/modules/productrelation/ProductRelationRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 public interface ProductRelationRepository extends JpaRepository<ProductRelation, Long> {
 
-    @EntityGraph(attributePaths = "product")
+    @EntityGraph(attributePaths = {"product","fromCharacter","toCharacter"})
     List<ProductRelation> findAllByProduct(Product product);
 
     @EntityGraph(attributePaths = {"fromCharacter","toCharacter"})
