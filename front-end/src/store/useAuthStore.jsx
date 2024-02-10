cd62472c41130c73dd8d00b82e03f9b747c3e1f9
seojeong <sjytis14@naver.com>
2024-02-10 11:18:18 +0900

    Fix: user default 값 변경

--- front-end/src/store/useAuthStore.jsx
+++ front-end/src/store/useAuthStore.jsx
@@ -3,13 +3,7 @@ import { devtools } from "zustand/middleware";
 
 export const useAuthStore = create(
   devtools((set) => ({
-    user: {
-      userNickname: "google_100945562307024095454",
-      userImageUrl: "",
-      email: "sjytis14@gmail.com",
-      userPhone: null,
-      userInfo: null,
-    },
+    user: null,
     setUser: (user) => {
       set({ user: user });
     },
