47d8cf990f495f3e3a7252d67f1c1e09885b8556
심지연 <sim.yyeon@gmail.com>
2024-02-12 02:38:53 +0900

    Feat: add getFshadowingList

--- back-end/src/main/java/com/example/backend/modules/story/StoryController.java
+++ back-end/src/main/java/com/example/backend/modules/story/StoryController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,7 +26,7 @@ public class StoryController {
                                                    @PathVariable("plotId") Long plotId) {
         //story 등록
         Story story = storyService.createStory(
-                StoryRequestDto.of(storyRequestDto,null,null),
+                StoryRequestDto.of(storyRequestDto, null, null),
                 plotId,
                 storyRequestDto.getStoryContent(),
                 storyRequestDto.getCharacters().stream().map(CharacterRequestDto::from).collect(Collectors.toList()),
@@ -39,7 +40,7 @@ public class StoryController {
     public ApiResult<StoryResponseDto> updateStory(@RequestBody StoryRequestDto storyRequestDto,
                                                    @PathVariable("storyId") Long storyId) {
         Story story = storyService.updateStory(storyId,
-                StoryRequestDto.of(storyRequestDto,null,null),
+                StoryRequestDto.of(storyRequestDto, null, null),
                 storyRequestDto.getCharacters().stream().map(CharacterRequestDto::from).collect(Collectors.toList()),
                 storyRequestDto.getForeShadowings().stream().map(ForeShadowingRequestDto::from).collect(Collectors.toList()));
         log.info("==========story update 완료=============");
@@ -55,20 +56,36 @@ public class StoryController {
 
     @GetMapping("/{storyId}")
     public ApiResult<StoryResponseDto> getStory(@PathVariable("storyId") Long storyId) {
-        StoryResponseDto storyResponseDto=storyService.findByStoryId(storyId);
+        StoryResponseDto storyResponseDto = storyService.findByStoryId(storyId);
         //storyResponseDto 복선에 제대로 된 값 넣기
-        List<ForeShadowingResponseDto> foreShadowingResponseDtos =  storyResponseDto.getForeShadowings();
-        for(ForeShadowingResponseDto fsdto: foreShadowingResponseDtos){
-//            fsdto.
-        }
+        List<ForeShadowingResponseDto> foreShadowingResponseDtos = storyResponseDto.getForeShadowings();
 
         return ApiResult.OK(storyResponseDto);
+    }
+
+    @GetMapping("/{storyId}/fshadowlist")
+    public ApiResult<List<ForeShadowingResponseDto>> getFshadowingList(@PathVariable("storyId") Long storyId) {
+        List<ForeShadowing> foreShadowings = storyService.findFshadowList(storyId);
+        List<ForeShadowingResponseDto> responseDtos = new ArrayList<>();
+        for (ForeShadowing fs : foreShadowings) {
+            List<FshadowStoryIdDto> storyids = foreShadowingService.findStories(fs);
+            log.info("==============복선으로 스토리 아이디리스트 받기===============");
+            responseDtos.add(ForeShadowingResponseDto.builder()
+                    .storyIdList(storyids)
+                    .columnId(setColumn(storyids, fs))
+                    .fShadowId(fs.getId())
+                    .fshadowContent(fs.getFShadowContent())
+                    .fshadowName(fs.getFShadowName())
+                    .fshadowClose(fs.getFShadowClose())
+                    .build());
+        }
+        return ApiResult.OK(responseDtos);
     }
 
     @PutMapping("/{storyId}/vertical")
     public ApiResult<StoryResponseDto> updatePositionY(@PathVariable("storyId") Long storyId,
                                                        @RequestBody StoryVerticalRequestDto storyVerticalRequestDto) {
-        storyService.updatePositionY(storyVerticalRequestDto.getStoryId(),storyVerticalRequestDto.getPositionY());
+        storyService.updatePositionY(storyVerticalRequestDto.getStoryId(), storyVerticalRequestDto.getPositionY());
         log.info("============y축 수정===========");
         StoryResponseDto storyResponseDto = storyService.findByStoryId(storyId);
         return ApiResult.OK(storyResponseDto);
