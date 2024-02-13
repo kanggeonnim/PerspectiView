63d04c76b9c3cd228bb1d9c77e9eb95075cba4f8
심지연 <sim.yyeon@gmail.com>
2024-02-13 21:11:23 +0900

    Feat: add jsoncreator

--- back-end/src/main/java/com/example/backend/modules/comment/CommentRequestDto.java
+++ back-end/src/main/java/com/example/backend/modules/comment/CommentRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.backend.modules.comment;
 
-import com.example.backend.modules.story.Story;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
@@ -18,5 +19,10 @@ public class CommentRequestDto {
         return Comment.builder()
                 .commentContent(commentRequestDto.getCommentContent())
                 .build();
+    }
+
+    @JsonCreator
+    public CommentRequestDto(@JsonProperty("commentContent") String commentContent){
+        this.commentContent = commentContent;
     }
 }
