0feaa66940632c1a93150d1bd4be4efbea93be6a
심지연 <sim.yyeon@gmail.com>
2024-02-14 01:01:43 +0900

    Test: db replication 전 설정으로 복귀

--- back-end/src/main/java/com/example/backend/infra/config/DataSourceConfig.java
+++ back-end/src/main/java/com/example/backend/infra/config/DataSourceConfig.java
@@ -1,56 +1,56 @@
-package com.example.backend.infra.config;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
-
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-public class DataSourceConfig {
-    private static final String MASTER_SERVER = "master";
-    private static final String SLAVE_SERVER = "slave";
-
-    @Bean
-    @Qualifier(MASTER_SERVER)
-    @ConfigurationProperties(prefix = "spring.datasource.master.hikari")
-    public DataSource masterDataSource() {
-        return DataSourceBuilder.create()
-                .build();
-    }
-
-    @Bean
-    @Qualifier(SLAVE_SERVER)
-    @ConfigurationProperties(prefix = "spring.datasource.slave.hikari")
-    public DataSource slaveDataSource() {
-        return DataSourceBuilder.create()
-                .build();
-    }
-
-    @Primary
-    @Bean
-    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
-        return new LazyConnectionDataSourceProxy(routingDataSource);
-    }
-
-    @Bean
-    public DataSource routingDataSource(
-            @Qualifier("masterDataSource") DataSource masterDataSource,
-            @Qualifier("slaveDataSource") DataSource slaveDataSource)
-    {
-        RoutingDataSource routingDataSourceImpl = new RoutingDataSource();
-        Map<Object, Object> targetDataSource = new HashMap<>();
-        targetDataSource.put("master", masterDataSource);
-        targetDataSource.put("slave", slaveDataSource);
-        routingDataSourceImpl.setTargetDataSources(targetDataSource);
-        routingDataSourceImpl.setDefaultTargetDataSource(masterDataSource);
-        return routingDataSourceImpl;
-    }
-
-}
+//package com.example.backend.infra.config;
+//
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+//
+//import javax.sql.DataSource;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class DataSourceConfig {
+//    private static final String MASTER_SERVER = "master";
+//    private static final String SLAVE_SERVER = "slave";
+//
+//    @Bean
+//    @Qualifier(MASTER_SERVER)
+//    @ConfigurationProperties(prefix = "spring.datasource.master.hikari")
+//    public DataSource masterDataSource() {
+//        return DataSourceBuilder.create()
+//                .build();
+//    }
+//
+//    @Bean
+//    @Qualifier(SLAVE_SERVER)
+//    @ConfigurationProperties(prefix = "spring.datasource.slave.hikari")
+//    public DataSource slaveDataSource() {
+//        return DataSourceBuilder.create()
+//                .build();
+//    }
+//
+//    @Primary
+//    @Bean
+//    public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+//        return new LazyConnectionDataSourceProxy(routingDataSource);
+//    }
+//
+//    @Bean
+//    public DataSource routingDataSource(
+//            @Qualifier("masterDataSource") DataSource masterDataSource,
+//            @Qualifier("slaveDataSource") DataSource slaveDataSource)
+//    {
+//        RoutingDataSource routingDataSourceImpl = new RoutingDataSource();
+//        Map<Object, Object> targetDataSource = new HashMap<>();
+//        targetDataSource.put("master", masterDataSource);
+//        targetDataSource.put("slave", slaveDataSource);
+//        routingDataSourceImpl.setTargetDataSources(targetDataSource);
+//        routingDataSourceImpl.setDefaultTargetDataSource(masterDataSource);
+//        return routingDataSourceImpl;
+//    }
+//
+//}
