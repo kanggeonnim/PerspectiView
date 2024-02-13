7f287bca1a341bc536b2d6bd7bc0385fdbfeca15
심지연 <sim.yyeon@gmail.com>
2024-02-13 14:33:50 +0900

    Feat: redis 설정

--- back-end/src/main/java/com/example/backend/modules/story/StoryResponseDto.java
+++ back-end/src/main/java/com/example/backend/modules/story/StoryResponseDto.java
@@ -3,7 +3,10 @@ package com.example.backend.modules.story;
 import com.example.backend.modules.character.Character;
 import com.example.backend.modules.character.CharacterResponseDto;
 import com.example.backend.modules.foreshadowing.ForeShadowing;
+import com.example.backend.modules.foreshadowing.ForeShadowingPreviewDto;
 import com.example.backend.modules.foreshadowing.ForeShadowingResponseDto;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -16,8 +19,8 @@ public class StoryResponseDto {
     private Long storyId;
     private String storyTitle;
     private List<CharacterResponseDto> characters;
-    private List<ForeShadowingResponseDto> foreShadowings;
-    private String storyContent;
+    private List<ForeShadowingPreviewDto> foreShadowings;
+    private ContentDto content;
     private int positionX;
     private Double positionY;
 
@@ -26,15 +29,39 @@ public class StoryResponseDto {
                 .storyId(story.getId())
                 .storyTitle(story.getTitle())
                 .characters(characters.stream().map(CharacterResponseDto::of).collect(Collectors.toList()))
-                .foreShadowings(foreShadowings.stream().map(foreShadowing ->
-                        ForeShadowingResponseDto.of(
-                                foreShadowing,
-                                null,
-                                "null?"
-                        )).collect(Collectors.toList()))
-                .storyContent(story.getContent().getContent())
+                .foreShadowings(foreShadowings.stream().map(foreShadowing -> ForeShadowingPreviewDto.of(foreShadowing)).collect(Collectors.toList()))
+                .content(ContentDto.of(story.getContent()))
                 .positionX(story.getPositionX())
                 .positionY(story.getPositionY())
                 .build();
+    }
+
+    public StoryResponseDto updateStoryResponseDto(Story story, List<CharacterResponseDto> characters, List<ForeShadowingPreviewDto> foreShadowings) {
+        this.storyId = story.getId();
+        this.storyTitle = story.getTitle();
+        this.characters = characters;
+        this.foreShadowings = foreShadowings;
+        this.content = ContentDto.of(story.getContent());
+        this.positionX = story.getPositionX();
+        this.positionY = story.getPositionY();
+
+        return this;
+    }
+
+    @JsonCreator
+    public StoryResponseDto(@JsonProperty("storyId") Long storyId,
+                            @JsonProperty("storyTitle") String storyTitle,
+                            @JsonProperty("characters") List<CharacterResponseDto> characters,
+                            @JsonProperty("foreShadowings") List<ForeShadowingPreviewDto> foreShadowings,
+                            @JsonProperty("content") ContentDto content,
+                            @JsonProperty("positionX") int positionX,
+                            @JsonProperty("positionY") Double positionY) {
+        this.storyId = storyId;
+        this.storyTitle = storyTitle;
+        this.characters = characters;
+        this.foreShadowings = foreShadowings;
+        this.content = content;
+        this.positionX = positionX;
+        this.positionY = positionY;
     }
 }
