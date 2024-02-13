685751e396ee17d31f7f96de6916573815c6f69f
심지연 <sim.yyeon@gmail.com>
2024-02-13 21:11:50 +0900

    Feat: add findWithUserByStory entitygraph

--- back-end/src/main/java/com/example/backend/modules/comment/CommentRepository.java
+++ back-end/src/main/java/com/example/backend/modules/comment/CommentRepository.java
@@ -1,10 +1,12 @@
 package com.example.backend.modules.comment;
 
 import com.example.backend.modules.story.Story;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-    List<Comment> findByStory(Story story);
+    @EntityGraph(attributePaths = {"user"})
+    List<Comment> findWithUserByStory(Story story);
 }
