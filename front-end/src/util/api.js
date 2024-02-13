4e8a1460387d9001062a969e3003383446e925d1
우재하 <jaeha6049@naver.com>
2024-02-13 17:45:53 +0900

    Fix: api.js 수정

--- front-end/src/util/api.js
+++ front-end/src/util/api.js
@@ -14,9 +14,9 @@ export const privateApi = axios.create({
   baseURL: VITE_BASE_URL,
   //TODO 머지할떄 조심
   headers: {
-    // Authorization: getCookie("accessToken"),
-    Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnb29nbGVfMTEyMTY1ODc3Njg3MTk0MDM1MDU3Iiwicm9sZSI6Ilt7XCJpZFwiOjMsXCJyb2xlXCI6XCJST0xFX1VTRVJcIn1dIiwiaWF0IjoxNzA3ODEyNzIxLCJleHAiOjE3MDc4MTk5MjF9.-py8rV47Vj4Tyceg0DJBxJj1BuCKMCYU7g5DiJ2B9yE",
+    Authorization: getCookie("accessToken"),
+    // Authorization:
+    // "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnb29nbGVfMTEyMTY1ODc3Njg3MTk0MDM1MDU3Iiwicm9sZSI6Ilt7XCJpZFwiOjMsXCJyb2xlXCI6XCJST0xFX1VTRVJcIn1dIiwiaWF0IjoxNzA3ODEyNzIxLCJleHAiOjE3MDc4MTk5MjF9.-py8rV47Vj4Tyceg0DJBxJj1BuCKMCYU7g5DiJ2B9yE",
     "Access-Control-Allow-Origin": "http://localhost:5173",
     "Access-Control-Allow-Credentials": true,
     "Content-Type": "application/json",
