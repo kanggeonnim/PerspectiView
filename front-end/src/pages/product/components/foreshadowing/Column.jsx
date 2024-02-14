af09c084f2b55b48ded7871e185a63b80d9c56dc
Sooji Kwon <gkdrhd6788@gmail.com>
2024-02-15 02:01:36 +0900

    Feat: Doing 복선 디자인

--- front-end/src/pages/product/components/foreshadowing/Column.jsx
+++ front-end/src/pages/product/components/foreshadowing/Column.jsx
@@ -4,7 +4,7 @@ import { ForeshadowingCard } from "./ForeshadowingCard";
 
 export default function Column({ column, colFshadows }) {
   return (
-    <div className="flex flex-col items-center w-full h-full mx-1">
+    <div className="flex flex-col items-center w-full h-full mx-1 border border-green-500 min-w-40">
       <div className="box-border flex justify-start w-full my-1">
         <h3 className="mx-2 text-lg font-semibold">{column.title}</h3>
         <Badge className="text-yellow-700 bg-yellow-300 rounded-full">
@@ -12,7 +12,7 @@ export default function Column({ column, colFshadows }) {
         </Badge>
       </div>
       <ScrollArea className="flex flex-col items-center w-full m-2 overflow-y-auto h-96 ">
-        <div className="p-2 min-h-24">
+        <div className="w-full p-2 min-h-24">
           {colFshadows.map((colFshadow, index) => (
             <ForeshadowingCard
               key={colFshadow.fshadowId}
