2e01c6963675a15fdc3728b8170807467fc51461
Sooji Kwon <gkdrhd6788@gmail.com>
2024-02-14 23:43:51 +0900

    Feat: ButtonSelect 머지충돌 해결

--- front-end/src/pages/workspace/components/selects/ButtonSelect.jsx
+++ front-end/src/pages/workspace/components/selects/ButtonSelect.jsx
@@ -1,38 +1,28 @@
 import { Badge } from "@/components/ui/badge";
 import { useState } from "react";
 
-export default function Buttonselect({ isEditing, onSelect, selectedGenres }) {
-  // 체크 버튼
-  // 장르
+export default function Buttonselect({ isEditing }) {
   const arr = [
     { id: 1, name: "SF" },
     { id: 2, name: "액션" },
     { id: 3, name: "로맨스" },
     { id: 4, name: "드라마" },
   ];
-
-  const handleSelect = (item) => {
-    const isSelected = selectedGenres?.some((genre) => genre.id === item.id);
-    const updatedGenres = isSelected
-      ? selectedGenres.filter((genre) => genre.id !== item.id)
-      : [...selectedGenres, item];
-    onSelect(updatedGenres);
-  };
-
-  return arr.map((item) => (
+  const [pick, setPick] = useState(arr);
+  const [select, setSelect] = useState([]);
+  console.log(select);
+  return pick.map((item) => (
     <div key={item.id}>
       <Badge
-        className="cursor-pointer"
-        onClick={() => handleSelect(item)}
-        variant={selectedGenres?.some((genre) => genre.id === item.id) ? "destructive" : "off"}
-        // onClick={() => {
-        //   if (isEditing) {
-        //     !select.includes(item)
-        //       ? setSelect((select) => [...select, item])
-        //       : setSelect(select.filter((button) => button !== item));
-        //   }
-        // }}
-        // variant={select.includes(item) ? "destructive" : "off"}
+        className="cursor-pointer "
+        onClick={() => {
+          if (isEditing) {
+            !select.includes(item)
+              ? setSelect((select) => [...select, item])
+              : setSelect(select.filter((button) => button !== item));
+          }
+        }}
+        variant={select.includes(item) ? "destructive" : "off"}
       >
         {item.name}
       </Badge>
