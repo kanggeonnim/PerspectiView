4e5bdf89a0b50f44606177e76ed44be7e4333279
우재하 <jaeha6049@naver.com>
2024-02-15 10:36:18 +0900

    Fix: 실행 기능 우선에 따라 검색창 삭제

--- front-end/src/util/api.js
+++ front-end/src/util/api.js
@@ -14,9 +14,9 @@ export const privateApi = axios.create({
   baseURL: VITE_BASE_URL,
   //TODO 머지할떄 조심
   headers: {
-    Authorization: getCookie("accessToken"),
-    // Authorization:
-    // "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnb29nbGVfMTEyMTY1ODc3Njg3MTk0MDM1MDU3Iiwicm9sZSI6Ilt7XCJpZFwiOjMsXCJyb2xlXCI6XCJST0xFX1VTRVJcIn1dIiwiaWF0IjoxNzA3NzkwNjk0LCJleHAiOjE3MDc3OTc4OTR9.r15y5Ds02II8lCcrO9iV7WoooglpZ8hJyYgpCim2aYs",
+    // Authorization: getCookie("accessToken"),
+    Authorization:
+    "Bearer+eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnb29nbGVfMTEyMTY1ODc3Njg3MTk0MDM1MDU3Iiwicm9sZSI6Ilt7XCJpZFwiOjMsXCJyb2xlXCI6XCJST0xFX1VTRVJcIn1dIiwiaWF0IjoxNzA3OTYwNjAyLCJleHAiOjE3MDc5Njc4MDJ9.Mus0syir5Nu2HunrizSf76Hhc1E3heZd2D_aW38EZf4",
     "Access-Control-Allow-Origin": "http://localhost:5173",
     "Access-Control-Allow-Credentials": true,
     "Content-Type": "application/json",
